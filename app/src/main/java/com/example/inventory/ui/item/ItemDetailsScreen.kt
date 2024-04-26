37c725b44443e0fae3b9c2817b853913aec3ded5
sirmong <sirmong@gmail.com>
2024-04-26 21:22:06 +0200

    feat: sell items

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
@@ -97,7 +97,9 @@ fun ItemDetailsScreen(
     ) { innerPadding ->
         ItemDetailsBody(
             itemDetailsUiState = uiState.value,
-            onSellItem = { },
+            onSellItem = {
+                viewModel.reduceQuantityByOne()
+            },
             onDelete = { },
             modifier = Modifier
                 .padding(innerPadding)
@@ -127,7 +129,7 @@ private fun ItemDetailsBody(
             onClick = onSellItem,
             modifier = Modifier.fillMaxWidth(),
             shape = MaterialTheme.shapes.small,
-            enabled = true
+            enabled = !itemDetailsUiState.outOfStock
         ) {
             Text(stringResource(R.string.sell))
         }
